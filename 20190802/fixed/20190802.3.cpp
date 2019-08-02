905f98992d8b6476a6c1bdaf02a4baaadba85252
DongXin <dong3gold@163.com>
2019-08-02 21:57:15 +0800

    init

---
+++ 20190802/fixed/20190802.3.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cctype>
+#include <stack>
+using namespace std;
+int a[100020];
+int f1[100020];
+int f2[1]
+int main(){
+	int n;
+	scanf("%d", &n);
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < n; j++){
+			scanf("%d", a[i] + j);
+		}
+	}
+	for(int i = 0; i < )
+}
